6665a5bbcaac1a6f72cab966addda8802074c230
Aaron Sky <aaronsky@skyaaron.com>
2016-04-13 15:29:32 -0400

    Rewrote resample logic and added millis() function

--- modulate.js
+++ modulate.js
@@ -1,81 +1,111 @@
-var five = require("johnny-five"),
-    board = new five.Board();
+const five = require('johnny-five'),
+      SAMPLE_WINDOW = 50,
+      MIC_PIN = 'A0',
+      SPEAKER_PIN = 2;
+    
+var board = new five.Board(),
+    sample = 0,
+    start = null;
 
-var normalizeMic = function (range, min, max, val) {
-    var num = range * (val-min)/(max - min);
+
+var normalizeMic = function(range, min, max, val) {
+    var num = range * (val - min) / (max - min);
     return num;
 };
 
-var toSpeakerFreq = function (val, inMin, inMax, outMin, outMax) {
+var toSpeakerFreq = function(val, inMin, inMax, outMin, outMax) {
     var range = outMax - outMin;
     var num = (val - inMin) / (inMax - inMin);
     var returnVal = (num > 0.5) ? 1 : 0;
     // var num = range * (val - inMin) / (inMax - inMin) + outMin;
-    return returnVal; 
+    return returnVal;
+};
+
+var millis = function () {
+    if (start instanceof Date) {
+        var current = new Date();
+        return current - start;
+    }
+    return 0;
+};
+
+var resample = function () {
+    var analogRead = Q.nfbind(this.analogRead),
+        startMillis = millis(),
+        peakToPeak = 0,
+        signalMax = 0,
+        signalMin = 1024,
+        working = 0;
+
+    while (millis() - startMillis < SAMPLE_WINDOW) {
+        working += 1;
+        analogRead(MIC_PIN)
+        .then(function (data) {
+            sample = data;
+            if (sample < 1024) {
+                if (sample > signalMax) {
+                    signalMax = sample;
+                } else if (sample < signalMin) {
+                    signalMin = sample;
+                }
+            }
+            working -= 1;
+        });
+    }
+    while (working > 0) {
+        console.log('sampling...');
+    }
+    peakToPeak = signalMax - signalMin;
+    volts = (peakToPeak * 3.3) / 1024;
+    return volts;
 };
 
 board.on("ready", function() {
-    var mic = new five.Sensor("A0");
-    var piezo = new five.Piezo(2);
-    var time = 16;
+    var mic = new five.Sensor(MIC_PIN),
+        piezo = new five.Piezo(SPEAKER_PIN);
+    start = new Date();
     
-    var signalMin = 1024;
-    var signalMax = 0;
-    var count = 0;
-    var peakToPeak = 0;
-    var volts = 0;
     
     // if above a certain threshold, output to speaker
-    
+
     // otherwise, calculate a threshold
-    this.wait(1, function() {
-        mic.on("data", function() {
-            if (count < 50) {
-                if (this.value < 1024) {
-                    if (this.value > signalMax) {
-                        signalMax = this.value;
-                    } else if (this.value < signalMin) {
-                        signalMin = this.value;
-                    }
-                }
-                count++;
-            } else {
-                peakToPeak = signalMax - signalMin;
-                volts = peakToPeak * 3.3 / 1024;
-                console.log(volts);       
-            }
+    mic.on('data', function () {
+        this.loop(1, function () {
+            console.log(resample);
         });
     });
+    
+    
     // peakToPeak = signalMax - signalMin;
     // var volts = peakToPeak * 3.3 / 1024;
     // console.log(volts);
-    
-    
-       // collect data for 50 mS
-//    while (millis() - startMillis < sampleWindow)
-//    {
-//       sample = analogRead(0);
-//       if (sample < 1024)  // toss out spurious readings
-//       {
-//          if (sample > signalMax)
-//          {
-//             signalMax = sample;  // save just the max levels
-//          }
-//          else if (sample < signalMin)
-//          {
-//             signalMin = sample;  // save just the min levels
-//          }
-//       }
-//    }
-//    peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
-//    double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
-    
-    
-    
+
+
+    // collect data for 50 mS
+    //    while (millis() - startMillis < sampleWindow)
+    //    {
+    //       sample = analogRead(0);
+    //       if (sample < 1024)  // toss out spurious readings
+    //       {
+    //          if (sample > signalMax)
+    //          {
+    //             signalMax = sample;  // save just the max levels
+    //          }
+    //          else if (sample < signalMin)
+    //          {
+    //             signalMin = sample;  // save just the min levels
+    //          }
+    //       }
+    //    }
+    //    peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+    //    double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+
+
+
     // this.wait(time, function() {
-        
+
     //     // if above a certain threshhold, output the value
-        
+
     //     // otherwise, use the data 
     //     mic.on("data", function() {
     //         // var num = normalizeMic(30, 330, 380, this.value);
@@ -84,5 +114,5 @@ board.on("ready", function() {
     //         (num === 1) ? piezo.frequency(988, time) : piezo.noTone(time);
     //     });
     // });
-    
+
 });
